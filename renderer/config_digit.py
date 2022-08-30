63134d58405ef7b26b2835cd83569cd7b017e836
Nicola Piga <nicolapiga@gmail.com>
2022-08-30 19:25:17 +0200

    Add configuration file for the Digit

---
+++ renderer/config_digit.py
@@ -0,0 +1,11 @@
+# Taken from https://github.com/facebookresearch/tacto/blob/main/tacto/config_digit.yml
+
+# camera parameters
+yfov = 60.0
+znear = 0.001
+
+# distance from the camera to the top point of gelpad
+cam2gel = 0.02166890464317989
+
+img_height = 320
+img_width = 240
